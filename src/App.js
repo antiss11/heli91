20604942433a166227d9d0304c94cf2f1de553ab
Dmitry Kulik <recc1337@gmail.com>
2023-06-16 08:53:55 +0300

    Add App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-function App() {
+import { useContext, useState } from "react";
+import { RouteContext, mapping } from "./Router";
+import Index from "./Pages/Index";
+import Form from "./Pages/Form";
+import Result from "./Pages/Result";
+
+function App(props) {
+  const [inputCount, setInputCount] = useState(0);
+  const [tareaCount, setTareaCount] = useState(0);
+  const [cboxCount, setCboxeCount] = useState(0);
+
+  const { page } = useContext(RouteContext);
+
   return (
-    <div className="App">
-    </div>
-  );
+    <>
+      { page === mapping.index && <Index /> }
+      { page === mapping.form && 
+        <Form updateInputCount={setInputCount} updateTareaCount={setTareaCount} updateCboxCount={setCboxeCount} />
+      }
+      { page === mapping.result && <Result {...{inputCount, tareaCount, cboxCount}} />}
+    </>
+
+  )
 }
 
 export default App;
